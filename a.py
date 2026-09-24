dad6fa666b7d24b3a82222e8954fef03d4175e74
eddiearam <enwaram@gmail.com>
2026-09-23 22:17:30 -0400

    Add 'Eddie Aram' to members list

--- a.py
+++ a.py
@@ -1,3 +1,3 @@
-members = ["Roxane Framroze", "Zainab Aamer", "Hyeyun Kwon"]
+members = ["Roxane Framroze", "Zainab Aamer", "Hyeyun Kwon", "Eddie Aram"]
 
 print(f"Hello, World! Our group members are: [{', '.join(members)}]")
